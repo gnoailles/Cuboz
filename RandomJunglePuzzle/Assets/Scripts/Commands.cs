b4cfff4544733f6fc4293919ad3f057145693c56
Guillaume Noailles <noailles.guillaume123@gmail.com>
2019-04-09 11:48:50 -0400

    Working inputs and controller

--- RandomJunglePuzzle/Assets/Scripts/Commands.cs
+++ RandomJunglePuzzle/Assets/Scripts/Commands.cs
@@ -20,7 +20,60 @@ public class MoveBackwardCommand : ICommand
 {
     void ICommand.Execute(GridMovement p_gridMovement)
     {
-        //p_gridMovement.MoveForward();
+        p_gridMovement.MoveBackward();
         Debug.Log("Move Backward");
+    }
+}
+
+public class MoveLeftCommand : ICommand
+{
+    void ICommand.Execute(GridMovement p_gridMovement)
+    {
+        p_gridMovement.MoveLeft();
+        Debug.Log("Move Left");
+    }
+}
+
+public class MoveRightCommand : ICommand
+{
+    void ICommand.Execute(GridMovement p_gridMovement)
+    {
+        p_gridMovement.MoveRight();
+        Debug.Log("Move Right");
+    }
+}
+public class DashForwardCommand : ICommand
+{
+    void ICommand.Execute(GridMovement p_gridMovement)
+    {
+        p_gridMovement.DashForward();
+        Debug.Log("Dash Forward");
+    }
+}
+
+public class DashBackwardCommand : ICommand
+{
+    void ICommand.Execute(GridMovement p_gridMovement)
+    {
+        p_gridMovement.DashBackward();
+        Debug.Log("Dash Backward");
+    }
+}
+
+public class DashLeftCommand : ICommand
+{
+    void ICommand.Execute(GridMovement p_gridMovement)
+    {
+        p_gridMovement.DashLeft();
+        Debug.Log("Dash Left");
+    }
+}
+
+public class DashRightCommand : ICommand
+{
+    void ICommand.Execute(GridMovement p_gridMovement)
+    {
+        p_gridMovement.DashRight();
+        Debug.Log("Dash Right");
     }
 }
